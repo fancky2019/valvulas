50a34b80471190f596d3bef4722decde6c442f5c
fancky <709737588@qq.com>
2019-06-11 12:45:53 +0800

    1

--- src/main/java/com/valvulas/fancky/controller/ProductController.java
+++ src/main/java/com/valvulas/fancky/controller/ProductController.java
@@ -2,18 +2,22 @@ package com.valvulas.fancky.controller;
 
 import com.valvulas.fancky.model.entity.Product;
 import com.valvulas.fancky.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class ProductController {
 
+    Logger logger=LoggerFactory.getLogger(ProductController.class);
 
     @Resource
     private ProductService productService;
@@ -21,8 +25,15 @@ public class ProductController {
     @RequestMapping("/getProducts")
     @ResponseBody
     public   List<Product> getProducts(Product product) {
-        //用对象
-        List<Product> list = productService.getProducts( product);
+        List<Product> list=new ArrayList<>() ;
+        try {
+            list = productService.getProducts( product);
+            logger.error("ddd");
+        }
+       catch (Exception ex)
+       {
+           logger.error(ex.getMessage());
+       }
         return list;
     }
 
