3e2528f96f91088014886b38078a9ee08ebeeffe
fancky <709737588@qq.com>
2019-08-01 10:08:50 +0800

    jar  war 切换

--- src/main/java/com/valvulas/fancky/controller/HomeController.java
+++ src/main/java/com/valvulas/fancky/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.valvulas.fancky.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.MessageFormat;
 import java.util.HashMap;
 
 @Controller
 //@RequestMapping("/home")
 public class HomeController {
+
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @RequestMapping("")
     public String Index(HashMap<String, Object> map, Model model) {
         model.addAttribute("say", "欢迎欢迎,热烈欢迎");
@@ -89,6 +101,21 @@ public class HomeController {
     @RequestMapping("/productDynamic")
     public String productDynamic() {
         return "product/dynamic";
+    }
+
+    @GetMapping("/log")
+    @ResponseBody
+    public String log(String str) {
+        logger.info(str);
+        return MessageFormat.format("{0}:{1}", applicationName, str);
+    }
+
+    //http://localhost:8080/login?name=fancky&password=pas
+    @GetMapping("/login")
+    @ResponseBody
+    public String login(String name,String password) {
+        logger.info(name+","+password);
+        return MessageFormat.format("{0}:{1}", applicationName, name+","+password);
     }
     //endregion
 }
