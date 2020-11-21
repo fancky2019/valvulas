c6f0179b04ed5224865b1ede6560f92190027cfd
fancky <709737588@qq.com>
2020-11-21 16:05:23 +0800

    1

---
+++ doc/v4.sql
@@ -0,0 +1,126 @@
+select *  from product;
+
+-- 68dd8719-8cc0-11e9-8f69-00163e320c36
+
+  INSERT INTO `valvulas`.`product` (
+  `GUID`,
+  `ProductName`,
+  `ProductStyle`,
+  `ImagePath`,
+  `CreadteTime`,
+  `ModifyTime`,
+  `Status`,
+  `Description`
+)
+VALUES
+  (
+    '68dd8719-8cc0-11e9-8f69-00163e320c02',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/2.jpg',
+   1560338163102,
+    1560338163102,
+    1,
+    'AirCompressorCheckValve2'
+  ),
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c03',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/3.jpg',
+   1560338163103,
+    1560338163103,
+    1,
+    'AirCompressorCheckValve3'
+  ) 
+    ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c04',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/4.jpg',
+   1560338163104,
+    1560338163104,
+    1,
+    'AirCompressorCheckValve4'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c05',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/5.jpg',
+   1560338163105,
+    1560338163105,
+    1,
+    'AirCompressorCheckValve5'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c06',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/6.jpg',
+   1560338163106,
+    1560338163106,
+    1,
+    'AirCompressorCheckValve6'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c07',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/7.jpg',
+   1560338163107,
+    1560338163107,
+    1,
+    'AirCompressorCheckValve7'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c08',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/8.jpg',
+   1560338163108,
+    1560338163108,
+    1,
+    'AirCompressorCheckValve8'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c09',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/9.jpg',
+   1560338163109,
+    1560338163109,
+    1,
+    'AirCompressorCheckValve9'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c10',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/10.jpg',
+   1560338163110,
+    1560338163110,
+    1,
+    'AirCompressorCheckValve10'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c11',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/11.jpg',
+   1560338163111,
+    1560338163111,
+    1,
+    'AirCompressorCheckValve11'
+  )  ,
+ (
+    '68dd8719-8cc0-11e9-8f69-00163e320c12',
+    'airCompressorCheckValve',
+    'airCompressorCheckValve',
+    '/img/airCompressorCheckValve/12.jpg',
+   1560338163112,
+    1560338163112,
+    1,
+    'AirCompressorCheckValve12'
+  )  ;
